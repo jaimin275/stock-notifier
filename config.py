120d171ed05eaa1d8fe1b4280be7c080f41be082
jaimin patel <jaimin2751997@gmail.com>
2021-01-11 23:31:07 +0530

    added some support for ebuyer retailer site [WIP]

--- config.py
+++ config.py
@@ -1,3 +1,24 @@
-selenium = {
-    "chromeDriverPath": "webdrivers/chromedriver_87_0_4280_88_win32.exe"
+config = {
+    'selenium': {
+        "chromeDriverPath": "webdrivers/chromedriver_87_0_4280_88_win32.exe"
+    },
+    'graphicsCards': ['RTX 3060', 'RTX 3070', 'RTX 3080', 'RTX 3090', 'RX 6700', 'RX 6800 XT', 'RX 6900 XT'],
+    'retailers': [
+        {
+            'name': 'ebuyer',
+            'site': 'https://www.ebuyer.com/'
+        },
+        {
+            'name': 'overclockers',
+            'site': 'https://www.overclockers.co.uk/'
+        },
+        {
+            'name': 'novatech',
+            'site': 'https://www.novatech.co.uk/'
+        },
+        {
+            'name': 'scan',
+            'site': 'https://www.scan.co.uk/'
+        }
+    ]
 }
---
+++ stock-notifier.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+
+from config import config
+
+def getChromeDriver(chromeDriverPath):
+    driver = webdriver.Chrome(chromeDriverPath)
+    return driver
+
+def findGraphicsCardsOnEbuyer(retailerConfig, driver):
+    driver.get(retailerConfig['site'])
+    try:
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "search-box"))
+        )
+        element.clear()
+        element.send_keys('RTX 3060')
+        element.send_keys(Keys.ENTER)
+
+        itemElement = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "search-box"))
+        )
+    finally:
+        driver.quit()
+
+def main():
+    # get the chrome driver for executing selenium code
+    driver = getChromeDriver(config['selenium']['chromeDriverPath'])
+
+    for retailer in config['retailers']:
+        if(retailer['name']=='ebuyer'):
+            findGraphicsCardsOnEbuyer(retailer, driver)
+        else:
+            pass
+
+ 
