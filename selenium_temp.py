ed2e80ea2300e927fa5d0d79df8acddd9e56fdc2
jaimin patel <jaimin2751997@gmail.com>
2021-01-11 16:37:20 +0530

    configured project for selenium support in local for chrome driver version 87

---
+++ selenium_temp.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+import config 
+
+
+driver = webdriver.Chrome(config.selenium['chromeDriverPath'])
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+time.sleep(5)
+assert "No results found." not in driver.page_source
+driver.close()
