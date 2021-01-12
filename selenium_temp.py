6c5901eaaffbfa43ce71d781d21fd67a6bfebc3f
jaimin patel <jaimin2751997@gmail.com>
2021-01-12 21:10:35 +0530

    added debugger for python file

--- selenium_temp.py
+++ selenium_temp.py
@@ -2,10 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-import config 
+from config import config 
 
 
-driver = webdriver.Chrome(config.selenium['chromeDriverPath'])
+driver = webdriver.Chrome(config['selenium']['chromeDriverPath'])
 driver.get("http://www.python.org")
 assert "Python" in driver.title
 elem = driver.find_element_by_name("q")
